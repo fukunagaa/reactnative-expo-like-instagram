7134732525628b54afd37407015247c14d2ad973
fukunagaa <k.k.0627ku@gmail.com>
2020-08-07 14:03:18 +0900

    NavigationContainer, Stackまで

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import UserScreen from "../screens/UserScreen";
 
+const Stack = createStackNavigator();
+
 const AppNavigator = () => {
-  return <UserScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            title: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default AppNavigator;
