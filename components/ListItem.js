6cfa8a4a66b315c760c68b57a68cea1760f1cb88
fukunagaa <k.k.0627ku@gmail.com>
2020-08-30 16:50:44 +0900

    画像の表示(styles)を変更

--- components/ListItem.js
+++ components/ListItem.js
@@ -5,15 +5,18 @@ import articles from "../dummies/articles.json";
 const styles = StyleSheet.create({
   container: {
     flex: 6,
-    flexDirection: "row",
-    flexWrap: "wrap",
+    flexDirection: "column",
     backgroundColor: "#FFFFFF",
-    alignContent: "flex-start",
-    justifyContent: "space-around",
+  },
+  articlesContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "red",
   },
   square: {
-    width: 130,
-    height: 130,
+    width: "100%",
+    flex: 1,
+    aspectRatio: 1,
   },
   itemImage: {
     width: "100%",
@@ -22,10 +25,64 @@ const styles = StyleSheet.create({
 });
 
 const ListItem = () => {
-  const listItem = articles.map((article, index) => {
+  let listArticles = [];
+  articles.map((article, index) => {
+    if (index % 3 == 0) {
+      listArticles.push({
+        article1: null,
+        article2: null,
+        article3: null,
+        idx1: 0,
+        idx2: 0,
+        idx3: 0,
+      });
+      listArticles[listArticles.length - 1].article1 = article;
+      listArticles[listArticles.length - 1].idx1 = index;
+    } else if (index % 3 == 1) {
+      listArticles[listArticles.length - 1].article2 = article;
+      listArticles[listArticles.length - 1].idx2 = index;
+    } else if (index % 3 == 2) {
+      listArticles[listArticles.length - 1].article3 = article;
+      listArticles[listArticles.length - 1].idx3 = index;
+    }
+  });
+  const listItem = listArticles.map((articles, index) => {
+    const el1 = (
+      <View style={styles.square} key={articles.idx1}>
+        <Image
+          style={styles.itemImage}
+          source={{ uri: articles.article1.urlToImage }}
+        />
+      </View>
+    );
+    const el2 =
+      articles.article2 == null ? (
+        <View style={styles.square} key="empty2"></View>
+      ) : (
+        <View style={styles.square} key={articles.idx2}>
+          <Image
+            style={styles.itemImage}
+            source={{ uri: articles.article2.urlToImage }}
+          />
+        </View>
+      );
+    const el3 =
+      articles.article3 == null ? (
+        <View style={styles.square} key="empty1"></View>
+      ) : (
+        <View style={styles.square} key={articles.idx3}>
+          <Image
+            style={styles.itemImage}
+            source={{ uri: articles.article3.urlToImage }}
+          />
+        </View>
+      );
+    console.log(articles.article3);
     return (
-      <View style={styles.square} key={index}>
-        <Image style={styles.itemImage} source={{ uri: article.urlToImage }} />
+      <View key={index} style={styles.articlesContainer}>
+        {el1}
+        {el2}
+        {el3}
       </View>
     );
   });
